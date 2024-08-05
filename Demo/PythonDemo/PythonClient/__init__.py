fda2f24c26c8e3d622f920b03e9a4c1d709abd3d
anshjoseph <ranshjosrph@gmail.com>
2024-08-05 19:00:59 +0530

    making new client lib init

--- Demo/PythonDemo/PythonClient/__init__.py
+++ Demo/PythonDemo/PythonClient/__init__.py
@@ -128,92 +128,20 @@ class PyGlobalInterface_Client:
         self.function_runner.cancel("time to stop")
 
 class BaseClient:
-    def __init__(self,port:int=9800,host:str="127.0.0.1") -> None:
+    def __init__(self,host:str,port:int) -> None:
         self.port = port
         self.host = host
-
-        self.reader:StreamReader = None
-        self.writter:StreamWriter = None
-        
-        self.connection_status = False
-        self.buffer = 4000
-        
-        self.loop = asyncio.new_event_loop()
-        self.loop.run_until_complete(self.connect())
-        self.client_name:str = None
-
         self.sending_queue:Queue = Queue()
         self.receving_queue:Queue = Queue()
-        
-
-
-        self.recever_task = self.loop.create_task(self.__sender())
-        self.sender_task = self.loop.create_task(self.__recever())
-        self.function_runner = self.loop.create_task(self.__function_prcessing())
-
-
-        self.function_register_hashmap:dict = dict()
-        self.function_called_task_hashmap:dict = dict()
-    async def __sender(self):
-        while True:
-            data:str = json.dumps( await self.sending_queue.get())
-            await self.writter.write(data.encode())
-    async def __recever(self):
-        while True:
-            data:dict = json.loads(self.reader.read(self.buffer))
-            await self.receving_queue.put(data)
-    
-    async def connect(self):
         try:
-            self.reader,self.writter = await open_connection(host=self.host,port=self.port)
+            self.reader, self.writter = asyncio.run(open_connection(host=self.host,port=self.port))
             self.connection_status = True
         except:
             self.connection_status = False
-    def register(self,name:str):
-        if self.connection_status:
-            self.client_name = name
-            self.sending_queue.put_nowait(
-                {
-                    "event":ClientEvent.CLIENT_REGISTER,
-                    "client-name":self.client_name
-                }
-            )
-            data = self.loop.run_until_complete(self.receving_queue.get())
-            if data['event'] == ClientEvent.CLIENT_REGISTER_SUCC:
-                return True
-            return False
-class ServerClient(BaseClient):
-    def __init__(self, port: int = 9800, host: str = "127.0.0.1") -> None:
-        super().__init__(port, host)
-        self.function_runner = self.loop.create_task(self.__function_processing())
-        self.function_register_hashmap:dict = dict()
-        self.function_call_queue:Queue = Queue()
-        self.thread_list = []
+        self.sender_task = asyncio.create_task(self.__sender_task())
+    
+    async def __sender_task(self):
+        pass
 
-    def func_register(self,function_name:str,function_ref:function):
-        if self.connection_status:
-            self.sending_queue.put_nowait({"event":ClientEvent.CLEINT_FUNCTION_REGISTER,"function-name":function_name})
-            data = self.loop.run_until_complete(self.receving_queue.get())
-            if data['event'] == ClientEvent.CLEINT_FUNCTION_REGISTER_SUCC:
-                self.function_register_hashmap[function_name] = function_ref
-                return True
-            elif data['event'] == ClientEvent.CLEINT_FUNCTION_REGISTER_FAIL:
-                return False
-            return False
-    def __function_runner(self,data:dict):
-        __ = time.time()
-        output = self.function_register_hashmap.get(data['function_name'])(data["data"])
-        print(output)
-        print( time.time() -__ )
-        self.sending_queue.put_nowait({"event":"func-ret","data":output,"to_client":data["to_client"],"task_id":data['task_id']})
-    
-    async def __function_processing(self):
-        while True:
-            data:dict = await self.function_call_queue.get()
-            async def __task(data):
-                thread = Thread(target=self.__function_runner,args=(data,))
-                thread.start()
-                while thread.is_alive():
-                    await asyncio.sleep(0.05)
-            self.thread_list.append(self.loop.create_task(__task(data)))
-    
+
+
